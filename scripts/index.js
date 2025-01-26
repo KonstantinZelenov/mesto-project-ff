65c8a60e1a8b09929540d2fe78052c54de9d8e12
Zelenov Konstantin <KonstantinZelenovViga@yandex.com>
2025-01-26 15:33:13 +0400

    add JS functions createCard, deleteCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function createCard(card, deleteCard) {
+  // Клонирую шаблон
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+  // Устанавливаю значения вложенных элементов
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__image').src = card.link;
 
-// @todo: Функция удаления карточки
+  // Добавляю обработчик клика к иконке удаления
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
+
+// Вывожу все карточки из массива на страницу
+initialCards.forEach(card => {
+  const cardElement = createCard(card, deleteCard);
+  placesList.append(cardElement);
+});
+
+
+
